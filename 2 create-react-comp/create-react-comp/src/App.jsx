893d34f4a52963326372ebeade7710575fdd6a3b
megha singh <meghasingh1045@gmail.com>
2024-08-18 17:18:16 +0530

    React project

--- 2 create-react-comp/create-react-comp/src/App.jsx
+++ 2 create-react-comp/create-react-comp/src/App.jsx
@@ -1,5 +1,6 @@
-import Mybutton from "./MyButton"
-
+import Mybutton from "./MyButton";
+import Hello from "./Hello";
+import Random from "./RandomNumber";
 
 //define functional components
 function App (){
@@ -7,6 +8,9 @@ return <div><h1>
   Hello react!
 </h1>
   <Mybutton></Mybutton>
+  <Hello></Hello>
+  <Random></Random>
+  <Random></Random>
 </div>
 
 }
---
+++ 2 create-react-comp/create-react-comp/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+
+let myName = 'megha';
+let fullName = 'megha singh'
+let number = 4675;
+    return <h3>Hello i am from the future , and my name is {fullName} ,the message no.is {number}</h3>
+}
+
+export default Hello;
